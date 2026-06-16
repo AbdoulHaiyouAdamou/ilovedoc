9a3b987739ccda8dc05dde99f17d4dbe81506016
Abdoul Haiyou Adamou Noma <abdoulhaiyouadamounoma@gmail.com>
2026-06-16 14:18:06 +0100

    feat: explicitly enable auto-locale detection based on user browser language

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -4,7 +4,8 @@ import {createNavigation} from 'next-intl/navigation';
 export const routing = defineRouting({
   locales: ['fr', 'en', 'es', 'de', 'it', 'pt', 'ja', 'ru', 'ko', 'zh', 'ar', 'bg', 'ca', 'nl', 'el', 'hi', 'id', 'ms', 'pl', 'sv', 'th', 'tr', 'uk', 'vi', 'sw', 'az', 'cs', 'da', 'eu', 'fa', 'ga', 'hr', 'hu', 'ml', 'no', 'ro', 'sk', 'sl', 'sr'],
   defaultLocale: 'fr',
-  localePrefix: 'as-needed'
+  localePrefix: 'as-needed',
+  localeDetection: true
 });
  
 export const {Link, redirect, usePathname, useRouter} =
